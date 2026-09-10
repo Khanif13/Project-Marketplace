79a30edd675c8bf473a4eb7c8d1c055fb7a8103c
khanif13 <ahmadhanif13125@gmail.com>
2026-09-10 15:13:18 +0800

    Fix Login Production Problem

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="mb-4 text-sm text-green-400 bg-green-500/10 border border-green-500/20 rounded-xl px-4 py-2.5">
             {{ session('status') }}
         </div>
-    @endifgit
+    @endif
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
